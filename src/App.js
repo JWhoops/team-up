03914a88c87e6e7f02b50d5907265e7a602270e4
Jinyu Wu <q835771840@gmail.com>
2020-05-15 22:41:53 -0500

    clean up and added navbar, still need to fix the icon and logo issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Navbar, Nav } from "react-bootstrap"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="dark" expand="md">
+      <Navbar.Brand href="#home">TEAM UP</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link className="navbarBtn" href="#recruite">招募</Nav.Link>
+          <Nav.Link href="#progress">进展</Nav.Link>
+          <Nav.Link href="#finish">完成</Nav.Link>
+        </Nav>
+        <div id="navbar-right-container" inline>
+          <div id="username-lbl" >da个大西瓜</div>
+          <div id="user-avatar"></div>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
