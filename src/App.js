be48a1c6c7bb38607a2c72ce8315841ee5f07cfc
Jinyu Wu <q835771840@gmail.com>
2020-05-16 23:08:18 -0500

    refactor header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Navbar, Nav } from "react-bootstrap"
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <Navbar bg="dark" expand="md">
-      <Navbar.Brand href="#home">TEAM UP</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link className="navbarBtn" href="#recruite">招募</Nav.Link>
-          <Nav.Link href="#progress">进展</Nav.Link>
-          <Nav.Link href="#finish">完成</Nav.Link>
-        </Nav>
-        <div id="navbar-right-container" inline>
-          <div id="username-lbl" >da个大西瓜</div>
-          <div id="user-avatar"></div>
-        </div>
-      </Navbar.Collapse>
-    </Navbar>
+    <Header />
   );
 }
 
