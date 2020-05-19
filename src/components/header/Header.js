1212c57032b9d73be280829888274945f50c2694
Jinyu Wu <q835771840@gmail.com>
2020-05-18 23:26:55 -0500

    almost finish router

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,13 +11,21 @@ function Header() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link to="/recruit">
-            <Link className="Navbar-btn" to="/recruit">招募</Link>
-          </Nav.Link>
-          {/* <Link className="navbarBtn" to="/finish">进展</Link>
-          <Link className="navbarBtn" to="/progress">完成</Link> */}
-          <Nav.Link href="/progress">进展</Nav.Link>
-          <Nav.Link href="/finish">完成</Nav.Link>
+          <Link className="Navbar-btn" to="/recruit">
+            <div className="nav-btn-wrapper">
+              招募
+            </div>
+          </Link>
+          <Link className="Navbar-btn" to="/progress">
+            <div className="nav-btn-wrapper">
+              进展
+            </div>
+          </Link>
+          <Link className="Navbar-btn" to="/finish">
+            <div className="nav-btn-wrapper">
+              完成
+            </div>
+          </Link>
         </Nav>
         <div id="navbar-right-container" inline>
           <div id="username-lbl" >da个大西瓜</div>
