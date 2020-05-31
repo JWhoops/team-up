ec30167b048f312aa30720eef312b9d8882e6fbf
Jinyu Wu <q835771840@gmail.com>
2020-05-31 16:59:42 -0500

    finish recruit detail page

--- src/components/pages/recruitDetail/RecruitDetailPage.js
+++ src/components/pages/recruitDetail/RecruitDetailPage.js
@@ -4,7 +4,7 @@ import './RecruitDetailPage.css';
 function RecruitDetailPage() {
     return (
         <div id="recruit-detail-container">
-            <div id="recruit-detail-general-info">
+            <div className="recruit-detail-info-boxes" id="recruit-detail-general-info">
                 <div>
                     <div id="general-info-head">
                         <h4 id="recruit-detail-movie-title">大头儿子</h4>
@@ -20,9 +20,34 @@ function RecruitDetailPage() {
                     <div id="recruit-detail-favorite-btn">Star</div>
                     <div id="recruit-detail-join-btn">申请加入</div>
                 </div>
-
             </div>
-        </div>
+            <div className="recruit-detail-info-boxes">
+                <p className="recruit-detail-sub-box">描述: <span>这个剧本超棒</span></p>
+            </div>
+            <div className="recruit-detail-info-boxes">
+                <p className="recruit-detail-sub-box">仍需: <span>1x导演，2x摄像</span></p>
+            </div>
+            <div className="recruit-detail-info-boxes ">
+                <p className="recruit-detail-sub-box ">组员:</p>
+                <div className="recruit-detail-member-container" >
+                    <div className="recruit-detail-member-card">
+                        <div className="recruit-detail-member-avatar"></div>
+                        <div className="recruit-detail-member-role">演员</div>
+                    </div>
+                    <div className="recruit-detail-member-card">
+                        <div className="recruit-detail-member-avatar"></div>
+                        <div className="recruit-detail-member-role">编剧</div>
+                    </div>
+                </div>
+            </div>
+            <div className="recruit-detail-info-boxes ">
+                <h5>评论:</h5>
+                <p className="recruit-detail-comment" ><span className="recruit-detail-comment-username">DDDDD:</span> 拍的很好</p>
+                <p className="recruit-detail-comment" > <span className="recruit-detail-comment-username">我是香蕉: </span>666666666</p>
+                <p className="recruit-detail-comment" > <span className="recruit-detail-comment-username">水儿: </span>我是水牙</p>
+                <div id="recruit-detail-comment-btn">添加评论</div>
+            </div>
+        </div >
     )
 }
 
