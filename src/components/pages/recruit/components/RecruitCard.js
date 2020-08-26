bf8feb44afedb1d4ae81e42e7e88d3d8665218bf
Jinyu Wu <q835771840@gmail.com>
2020-08-26 15:57:53 -0500

    adjusted styling & parameter for teampages

--- src/components/pages/recruit/components/RecruitCard.js
+++ src/components/pages/recruit/components/RecruitCard.js
@@ -2,14 +2,16 @@ import React from 'react';
 import "./RecruitCard.css"
 
 function Card(props) {
-    const { movie_title, wanted_roles, description, location } = props.team;
+    // const { movie_title, wanted_roles, description, location } = props.team;
+    const { movie_title, name, wanted_roles, description, location } = props.team;
     var roles = wanted_roles.map(r => r.number + "x" + r.role).join(", ")
 
     return (
         <div className="recruit-card">
             <h3 class="recruit-card-title">{movie_title}</h3>
-            <h6 class="recruit-card-location">{location} <br />
-                {roles}</h6>
+            <h6 class="recruit-card-location">{location}</h6>
+            <h6 class="recruit-card-username">{name}</h6>
+            <h6>招 {roles}</h6>
             <p id="recruit-card-description">{description}</p>
         </div>
     )
